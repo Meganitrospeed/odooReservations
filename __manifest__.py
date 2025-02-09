7bd148936e39c10de4956cbbc0f1e47031933691
Meganitrospeed <meganitrospeed@gmail.com>
2025-02-09 03:56:46 +0000

    Attempt to create a view for Calendar

--- __manifest__.py
+++ __manifest__.py
@@ -7,6 +7,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/appointment_views.xml',
+        'views/appointment_calendar_view.xml',
     ],
     'installable': True,
     'application': True,
